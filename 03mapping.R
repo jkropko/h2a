2c155804b179ec17e78b4c1fad08f2b045af9454
aslez <aslez@virginia.edu>
2023-05-11 10:58:10 -0400

    Added type, job title, and wages to pop-up. Changed symbol color to denote type.

--- 03mapping.R
+++ 03mapping.R
@@ -9,6 +9,8 @@ data$address <- paste(data$WORKSITE_ADDRESS, data$WORKSITE_CITY,
                       data$WORKSITE_STATE, data$WORKSITE_POSTAL_CODE)
 
 Encoding( x = data$address ) <- "UTF-8"
+Encoding( x = data$JOB_TITLE ) <- "UTF-8"
+Encoding( x = data$WAGE ) <- "UTF-8"
 
 # replace all non UTF-8 character strings with an empty space
 data$address <-
@@ -17,15 +19,30 @@ data$address <-
          , to = "UTF-8"
          , sub = "" )
 
+data$JOB_TITLE <-
+  iconv( x = data$JOB_TITLE
+         , from = "UTF-8"
+         , to = "UTF-8"
+         , sub = "" )
 
-data$popup <- paste('<b>Case No.:',data$CASE_NUMBER, '</b><br/>',
+data$WAGE <-
+  iconv( x = data$WAGE
+         , from = "UTF-8"
+         , to = "UTF-8"
+         , sub = "" )
+
+data$popup <- paste('<b>Type:', data$TYPE, '</b><br/>',
+                    '<b>Case No.:',data$CASE_NUMBER, '</b><br/>',
                     'Case status:', data$CASE_STATUS, '<br/>',
                     'Worksite address:', data$address, '<br/>',
-                    'Total worksites records:', data$TOTAL_WORKSITES_RECORDS)
+                    'Total worksites records:', data$TOTAL_WORKSITES_RECORDS, '<br/>',
+                    'Job title:', data$JOB_TITLE, '<br/>',
+                    'Wage:', data$WAGE)
                     
-map <- leaflet(data = data) %>% 
+sym_palette <- colorFactor(palette = 'RdBu', data$TYPE)
+map <-  leaflet(data = data) %>% 
   addTiles() %>%
-  addMarkers(~lon, ~lat, clusterOptions = markerClusterOptions(), 
-             popup = ~popup)
+  addCircleMarkers(~ lon, ~ lat, clusterOptions = markerClusterOptions(), 
+             popup = ~ popup, color = ~ sym_palette(TYPE))
 
 saveWidget(map, file="afl_cio_map.html")
